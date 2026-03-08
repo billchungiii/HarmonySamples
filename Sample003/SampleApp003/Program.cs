f2ad63d762d72fe7029ca5cf643e2f6f7d992b8d
Bill Chung <bill_chungiii@hotmail.com>
2026-03-08 17:57:51 +0800

    ass Sample003

---
+++ Sample003/SampleApp003/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using SampleLibrary003;
+namespace SampleApp003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var harmony = new Harmony("com.example.sampleApp003");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            string message = "default message";
+            message = OriginalClass.DisplayMessage("Hello, Harmony!", 10, 5);
+            Console.WriteLine(message);
+            Console.WriteLine("-----------------");
+            message= OriginalClass.DisplayMessage("Hello, Harmony!", 10, 10);
+            Console.WriteLine(message);
+        }
+    }
+
+    public static class PatchOriginal
+    {
+        [HarmonyPatch(typeof(OriginalClass), "Divide")]
+        public static class DisplayMessagePatch
+        {
+            public static bool Prefix(double x, double y, ref double __result)
+            {
+                if (y== 0)
+                {
+                    Console.WriteLine("Prefix: Detected potential divide by zero. Modifying y to prevent exception.");
+                    y = 1; // Modify y to prevent division by zero
+                    __result = 0;
+                    return false; // Skip the original method
+                }
+                return true;
+            }
+           
+        }
+    }
+}
---
+++ Sample003/SampleLibrary003/OriginalClass.cs
@@ -0,0 +1,28 @@
+﻿namespace SampleLibrary003
+{
+    public class OriginalClass
+    {
+        public static string DisplayMessage(string message, double a, double b)
+        {
+            double x = a;
+
+            /* 假設在程式碼內呼叫許多層的方法後計算後會讓 y=0 (if a==b) , 導致 Divide 方法拋出例外
+             這裡的計算只是為了模擬這種情況，實際上可能會有更複雜的邏輯導致 y=0 */
+            /* Assume that after calling multiple layers of methods in the code, the calculation results in y=0 (if a==b) ,  causing the Divide method to throw an exception. 
+               This calculation is only to simulate such a situation; in reality, there might be more complex logic leading to y=0. */
+            double y = a - b;   
+            double result = Divide(x, y);
+            return $"Original Message : {message}, Result: {result}";
+        }
+
+        private static double Divide(double x, double y)
+        {
+            Console.WriteLine("Executing Original Method...");
+            if (y == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return x / y;
+        }
+    }
+}

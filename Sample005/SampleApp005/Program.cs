1a2ffac8e405106cc23c291cfa5704f95661e8b7
Bill Chung <bill_chungiii@hotmail.com>
2026-03-09 04:29:48 +0800

    add project and modify nuget package

---
+++ Sample005/SampleApp005/Program.cs
@@ -0,0 +1,12 @@
+﻿using SampleLibrary005;
+
+namespace SampleApp005
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount { Owner = "Alice", Balance = 1000m, IsFrozen = false };
+        }
+    }
+}
---
+++ Sample005/SampleLibrary005/BankAccount.cs
@@ -0,0 +1,14 @@
+﻿namespace SampleLibrary005
+{
+    public class BankAccount
+    {
+        public string Owner { get; set; }
+        public decimal Balance { get; set; }
+        public bool IsFrozen { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+    }
+}

1f49581cf8a52ccc636b24027c6563409736b906
Bill Chung <bill_chungiii@hotmail.com>
2026-03-08 18:24:15 +0800

    modify PatchOriginal

--- Sample003/SampleApp003/Program.cs
+++ Sample003/SampleApp003/Program.cs
@@ -13,27 +13,22 @@ namespace SampleApp003
             message = OriginalClass.DisplayMessage("Hello, Harmony!", 10, 5);
             Console.WriteLine(message);
             Console.WriteLine("-----------------");
-            message= OriginalClass.DisplayMessage("Hello, Harmony!", 10, 10);
+            message = OriginalClass.DisplayMessage("Hello, Harmony!", 10, 10);
             Console.WriteLine(message);
         }
     }
-
+    [HarmonyPatch(typeof(OriginalClass), "Divide")]
     public static class PatchOriginal
     {
-        [HarmonyPatch(typeof(OriginalClass), "Divide")]
-        public static class DisplayMessagePatch
+        public static bool Prefix(double x, double y, ref double __result)
         {
-            public static bool Prefix(double x, double y, ref double __result)
+            if (y == 0)
             {
-                if (y== 0)
-                {
-                    Console.WriteLine("Prefix: Detected potential divide by zero. Modifying y to prevent exception.");
-                    __result = 0;
-                    return false; // Skip the original method
-                }
-                return true;
+                Console.WriteLine("Prefix: Detected potential divide by zero. Modifying y to prevent exception.");
+                __result = 0;
+                return false; // Skip the original method
             }
-           
+            return true;
         }
     }
 }

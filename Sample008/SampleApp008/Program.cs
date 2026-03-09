6f637e0cdfae2e6d1518ffb579527e59ab9b9934
Bill Chung <bill_chungiii@hotmail.com>
2026-03-10 00:41:46 +0800

    add smpale008

---
+++ Sample008/SampleApp008/Program.cs
@@ -0,0 +1,66 @@
+﻿using HarmonyLib;
+using SampleLibrary008;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace SampleApp008
+{
+    internal class Program
+    {
+        async static Task Main(string[] args)
+        {
+            var harmony = new Harmony("com.example.SampleApp008");
+            harmony.PatchAll();
+            await OriginalClass.LongTimeMethodAsync();
+        }
+    }
+
+    public class StatechMachineInfo
+    {
+        public FieldInfo Field { get; set; }
+        public Stopwatch Stopwatch { get; set; }
+    }
+
+
+    [HarmonyPatch]
+    public static class PatchOriginal
+    {
+        private static readonly ConditionalWeakTable<object, StatechMachineInfo> _stopwatches = new();
+        static MethodBase TargetMethod()
+        {
+            var method = typeof(OriginalClass).GetMethod(nameof(OriginalClass.LongTimeMethodAsync));
+            var attr = method.GetCustomAttribute<AsyncStateMachineAttribute>();
+            return attr.StateMachineType.GetMethod("MoveNext", BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        static void Prefix(object __instance)
+        {
+            if (!_stopwatches.TryGetValue(__instance, out var info))
+            {
+                // The state machine has an internal field called "<>1__state"
+                // -1 indicates the state machine has just started execution
+                var stateField = __instance.GetType().GetField("<>1__state", BindingFlags.Public | BindingFlags.Instance);
+                var sw = new Stopwatch();
+                info = new StatechMachineInfo { Field = stateField, Stopwatch = sw};
+                _stopwatches.Add(__instance, info);
+                sw.Start();
+            }
+        }
+
+        static void Postfix(object __instance)
+        {
+            if (_stopwatches.TryGetValue(__instance, out var info))
+            {
+                int internalState = (int)info.Field.GetValue(__instance);
+                if (internalState == -2) // State -2 indicates the async state machine has completed execution
+                {
+                    info.Stopwatch.Stop();
+                    Console.WriteLine($"[Harmony] LongTimeMethod 執行完畢，耗時: {info.Stopwatch.ElapsedMilliseconds} ms");
+                    _stopwatches.Remove(__instance);
+                }
+            }
+        }
+    }
+}
---
+++ Sample008/SampleLibrary008/OriginalClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+using System.Threading.Tasks;
+
+namespace SampleLibrary008
+{
+    public class OriginalClass
+    {
+        public static async Task LongTimeMethodAsync()
+        {
+            Console.WriteLine("executing LongTimeMethod.....");
+            await Task.Delay(200);
+        }
+    }
+}

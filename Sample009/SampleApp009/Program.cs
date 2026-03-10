2cd55dcc53c4b1593e9811170ba4cc64f0df821c
Bill Chung <bill_chungiii@hotmail.com>
2026-03-10 22:45:34 +0800

    add sample009

---
+++ Sample009/SampleApp009/Program.cs
@@ -0,0 +1,77 @@
+﻿using HarmonyLib;
+using SampleLibrary009;
+using System.Reflection;
+
+namespace SampleApp009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var harmony = new Harmony("com.example.sample009");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            var originalObject = new OriginalClass();
+            try
+            {
+                originalObject.DisplayMessage("Hello, Harmony!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Original class exception occurred: {ex.Message}");
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(OriginalClass), nameof(OriginalClass.DisplayMessage))]
+    public static class PatchOriginal
+    {
+        public static void Prefix(string message, ref OtherClass __state)
+        {
+            Console.WriteLine($"Prefix: About to display message: {message}");
+            __state = new OtherClass();
+        }
+
+        public static void Postfix(string message)
+        {
+            Console.WriteLine($"Postfix: Finished displaying message: {message}");
+        }
+
+        public static void Finalizer(ref OtherClass __state)
+        {
+            Console.WriteLine("Finalizer: Cleaning up resources.");
+            __state?.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// A class that simulates a resource that needs to be disposed.
+    /// Implements the IDisposable pattern for proper resource cleanup.
+    /// </summary>
+    public class OtherClass : IDisposable
+    {
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: Dispose managed state (managed objects)
+                }
+
+                // TODO: Free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: Set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+            Console.WriteLine("OtherClass disposed.");
+        }
+    }
+}
---
+++ Sample009/SampleLibrary009/OriginalClass.cs
@@ -0,0 +1,16 @@
+﻿namespace SampleLibrary009
+{
+    public class OriginalClass
+    {
+        /// <summary>
+        /// This method intentionally throws an exception after displaying the provided message.
+        /// </summary>
+        /// <param name="message">The message to be displayed before the exception is thrown.</param>
+        /// <exception cref="InvalidOperationException">Thrown to demonstrate exception handling.</exception>
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine($"Original Message : {message}");
+            throw new InvalidOperationException("An error occurred in DisplayMessage.");
+        }
+    }
+}

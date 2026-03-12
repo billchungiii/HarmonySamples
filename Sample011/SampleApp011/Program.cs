fbe66384134da3f08296bddd5772fb50c8b11f8b
Bill Chung <bill_chungiii@hotmail.com>
2026-03-13 03:00:21 +0800

    add comment

--- Sample011/SampleApp011/Program.cs
+++ Sample011/SampleApp011/Program.cs
@@ -16,6 +16,13 @@ namespace SampleApp011
         }
     }
 
+    /// <summary>
+    /// Provides a Harmony patch for the static constructor of the OriginalClass type.
+    /// </summary>
+    /// <remarks>This class is used to modify the static constructor of OriginalClass at runtime using
+    /// Harmony. It replaces any occurrence of the integer constant 100 with 5 in the constructor's IL code. This patch
+    /// is typically used to alter the behavior of the original static constructor without modifying the source code
+    /// directly.</remarks>
     [HarmonyPatch(typeof(OriginalClass), MethodType.StaticConstructor)]
     public class PatchOriginal
     {

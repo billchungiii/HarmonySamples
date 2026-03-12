1b9a594c8d6be8932a32beaa8d8b1bbd590d6168
Bill Chung <bill_chungiii@hotmail.com>
2026-03-13 00:06:43 +0800

    add sample011

---
+++ Sample011/SampleApp011/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using HarmonyLib;
+using SampleLibrary011;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace SampleApp011
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var harmony = new Harmony("com.example.sampleApp011");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            Console.WriteLine($"Original Value: {OriginalClass.Value}"); 
+        }
+    }
+
+    [HarmonyPatch(typeof(OriginalClass), MethodType.StaticConstructor)]
+    public class PatchOriginal
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {               
+                if (instruction.opcode == OpCodes.Ldc_I4_S && instruction.operand is sbyte val && val == 100)
+                {
+                    yield return new CodeInstruction(OpCodes.Ldc_I4_5);
+                }
+                else
+                {
+                    yield return instruction; 
+                }
+            }
+        }
+    }
+}
---
+++ Sample011/SampleLibrary011/OriginalClass.cs
@@ -0,0 +1,8 @@
+﻿namespace SampleLibrary011
+{
+    public class OriginalClass
+    {
+        private static readonly int value = 100;
+        public static int Value => value;
+    }
+}
